a0173cffae2752b5a459017ce81591762904db36
Abhinav Sorate <abhinavsorate01@gmail.com>
2023-04-17 23:18:13 +0530

    add microservice

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateUserEvent } from './events/create-user.event';
 
 @Injectable()
 export class AppService {
+  private readonly users: any[] = [];
+
+  constructor(
+    @Inject('COMMUNICATION') private readonly communicationClient: ClientProxy,
+  ) {} //injecting the 'COMMUNICATION' microservice
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createUser(dto: CreateUserDto) {
+    this.users.push(dto);
+    //emitting the event 'user_created'
+    this.communicationClient.emit(
+      'user_created',
+      new CreateUserEvent(dto.email),
+    );
   }
 }
